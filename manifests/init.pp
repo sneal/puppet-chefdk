c089d5d31de57eaa3dbf297de7efcd6f5286f62b
Shawn Neal <sneal@sneal.net>
2015-04-03 10:17:41 -0700

    Use dmg package installer and allow version overrides

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
-class chefdk{
-	require homebrewcask
-
-        exec { 'brew cask install chefdk': }
-
+class chefdk (
+  $version = '0.4.0',
+  $patch_level = '1'
+) {
+  package { "chefdk-${version}-${patch_level}":
+    ensure   => installed,
+    provider => 'pkgdmg',
+    source   => "https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/chefdk-${version}-${patch_level}.dmg",
+  }
 }
